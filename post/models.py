7c6bbc7336ccc872903e4d67e4e06930ce279ec9
ranil <ranilraj18@gmail.com>
2025-11-28 12:15:44 +0530

    data from database is inserted

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    img_url=models.URLField(max_length=300,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
--- post/views.py
+++ post/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.urls import reverse
+from .models import Post
+
 
 # Create your views here.
 def home(request):
-    title=[
-        {"title":"Post 1","content":"kjdfhgjdfhkldjdfklj"},
-        {"title":"Post 2","content":"kjdfhgjdfhkldjdfklj"},
-        {"title":"Post 3","content":"kjdfhgjdfhkldjdfklj"},
-        {"title":"Post 4","content":"kjdfhgjdfhkldjdfklj"},
-        {"title":"Post 5","content":"kjdfhgjdfhkldjdfklj"},
-        {"title":"Post 6","content":"kjdfhgjdfhkldjdfklj"}
-    ]
-    page_info={"title":"home","title_content":title}
-    return render(request,"index.html",context=page_info)
+    posts=Post.objects.all()
+    return render(request,"index.html",context={"post":posts})
  
 def details(request):
     return render(request,"details.html")
